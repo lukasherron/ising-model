df16764645d093c225a5c3fc4105f8a724b10f7b
lukasherron <64547197+lukasherron@users.noreply.github.com>
2020-05-29 11:58:09 -0400

    Added inputs to load a saved lattice

--- ising_args.py
+++ ising_args.py
@@ -8,6 +8,7 @@ Created on Thu Apr 30 12:16:42 2020
 import numpy as np
 import matplotlib.pyplot as plt
 import ising_microcanonical_defs as im
+import csv
 
 # CHANGE LOC TO "HPG" FOR HIPERGATOR SIMS
 # CHANGE LOC TO "local" FOR LOCAL SIMS
@@ -20,31 +21,50 @@ if LOC == "local":
     current_dir = '/home/lukas/Ising Research/'
     # Directory where scripts are located
 if LOC == "HPG":
-     data_dir = '\\ufrc\\pdixit\\lukasherron\\ising_data\\'
-     current_dir = '\\ufrc\\pdixit\\lukasherron\\ising_sim\\'
+     data_dir = '/ufrc/pdixit/lukasherron/ising_data/'
+     current_dir = '/ufrc/pdixit/lukasherron/ising_sim/'
     
-#%%
-     
-
-subsites = 4
+# INPUT PARAMETERS
+#-----------------------------------------------------------------------------
+subsites = 3
 N = 20
 m = 100
 s = 1000000
 n = int(s/m)
 # MUST INCLUDE AS 
 initial_dtype = 'temp'
+mode = "load"
 initial_mag = 0.8
-initial_temp = 2.27
+initial_temp = 2.2
+filename = "/home/lukas/Ising Research/Data/initial_lattices/20x20_initialized_lattice_temp=02.2_randint=586.txt"
+#-----------------------------------------------------------------------------
 
 if initial_dtype == 'mag':
-    lattice = im.initialize_lattice_mag(N, initial_mag)
-    initial_value = initial_mag
+    if mode != "load":
+        lattice = im.initialize_lattice_mag(N, initial_mag)
+        initial_value = initial_mag
+    else:
+         with open(filename, mode = 'r') as file:
+             lattice = csv.reader(file, delimiter='\t')
 if initial_dtype == 'temp':
-    lattice = im.initialize_lattice_temp(N, 1000, initial_temp)
-    initial_value = initial_temp
+     if mode != "load":
+         lattice = im.initialize_lattice_temp(N, 100, initial_temp)
+         initial_value = initial_temp
+     else:
+         with open(filename, mode = 'r') as file:
+             csv_lattice = list(csv.reader(file, delimiter='\t'))
+             N = (len(csv_lattice[0][:]))
+             lattice = np.zeros((N,N))
+             for i in range(N):
+                 for j in range(N):
+                     lattice[i][j] = csv_lattice[i][j]
+         i = filename.find('temp')
+         initial_value = float(filename[i+5:i+9])
+
+         
 
 time = np.linspace(0,s,int(s/m))
-demon_energy, upper_bound = 32, 64
+demon_energy, upper_bound = 16, 32
 
 lattice, energy, mag, de, sample_energy, sample_mag, s = \
             im.main(initial_dtype, initial_value, s, N, n, lattice, demon_energy,\
